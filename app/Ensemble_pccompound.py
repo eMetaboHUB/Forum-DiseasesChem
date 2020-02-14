8069efc451d18bacd5a881685f806c3458f3fefe
Maxime Delmas <maxime.delmas@inra.fr>
2020-02-14 15:53:06 +0100

    Add append_compound function to simplifly code

--- app/Ensemble_pccompound.py
+++ app/Ensemble_pccompound.py
@@ -1,19 +1,44 @@
 from Pccompound import Pccompound
+import eutils
+import numpy
+import xml.etree.ElementTree as ET
 class Ensemble_pccompound:
     """This class represent an ensembl of Pccompound objects, it's composed of:
     - a list of cid
     - a size attribute
     - a corresponding list of Pccompound objects
     """
-    def __init__(self, pccompound_list):
-        self.pccompound_list = pccompound_list        
-        self.size = len(pccompound_list)
+    def __init__(self):
+        self.pccompound_list = list()        
+        self.size = 0
+        self.append_failure = list()
+    
+    def append_pccompound(self, cid, query_builder):
+        pmids_by_source = {}
+        # Get CID associated PMID
+        response = query_builder.elink({"dbfrom": "pccompound", "db": "pubmed", "id": cid})
+        root = ET.fromstring(response)
+        # Exploring sets
+        for pmid_set in root.findall("./LinkSet/LinkSetDb"):
+            # Each source is assigned as a Key value and PMID list as values
+            pmids_by_source[(pmid_set.find("./LinkName").text)] = [set.text for set in pmid_set.findall("./Link/Id")]
+        # If no refenreces can be found for the cid, exit function and add it to append_failure list
+        if len(pmids_by_source) == 0:
+            self.append_failure.append(cid)
+            return None
+        # Create Union and prepare associated sources
+        pmids_union = list(set().union(*(pmids_by_source.values())))
+        sources = [list() for i in range(len(pmids_union))]
+        # For each PMID ressource in the union set, determine which are the orginals sources of the association.
+        for source in pmids_by_source.keys():
+            a = numpy.array(numpy.isin(pmids_union, pmids_by_source[source])).nonzero()
+            [sources[index].append(source) for index in a[0].tolist()]
+        self.pccompound_list.append(Pccompound(cid = cid, pmids = pmids_union, pmids_sources = sources))
     
     def export_cids_pmids_triples_ttl(self, output_file):
         # Preparing file and writing prefix
         f = open(output_file, "w")
         f.write("@prefix cito:\t<http://purl.org/spar/cito/> .\n@prefix compound:\t<http://rdf.ncbi.nlm.nih.gov/pubchem/compound/> .\n@prefix reference:\t<http://rdf.ncbi.nlm.nih.gov/pubchem/reference/> .\n")
-        # for (pcc in pccompound_list):
         for pcc in self.pccompound_list:
             pmid_export = " ,\n\t\t".join(["reference:PMID"+pmid for pmid in pcc.get_pmids()]) + " .\n"
             pmid_export = "compound:CID" + pcc.get_cid() + "\tcito:isDiscussedBy\t" + pmid_export
--- app/Get_CID_PMID_associations.py
+++ app/Get_CID_PMID_associations.py
@@ -1,13 +1,8 @@
 # Importing packages
 # Using Package eutils 0.6.0
 import eutils
-import numpy
-import xml.etree.ElementTree as ET
 from Ensemble_pccompound import Ensemble_pccompound
-from Pccompound import Pccompound
-from Pmid import Pmid
 
-np = numpy
 apiKey = "0ddb3479f5079f21272578dc6e040278a508"
 cid  = "6036"
 # Intialize the result list :
@@ -16,29 +11,8 @@ cids_pmids_list = list()
 query_builder = eutils.QueryService(cache = False,
                                     default_args ={'retmax': 100000, 'retmode': 'xml', 'usehistory': 'y'},
                                     api_key = apiKey)
-# Get CID associated PMID
-response = query_builder.elink({"dbfrom": "pccompound", "db": "pubmed", "id": cid})
-# parsing XML
-root = ET.fromstring(response)
-# For each CID - PMIS association source: "pccompound_pubmed", "pccompound_pubmed_mesh", "pccompound_pubmed_publisher"
-pmids_by_source = {}
-# Exploring sets
-for pmid_set in root.findall("./LinkSet/LinkSetDb"):
-    # Each source is assigned as a Key value and PMID list as values
-    pmids_by_source[(pmid_set.find("./LinkName").text)] = [set.text for set in pmid_set.findall("./Link/Id")]
-# Create Union and prepare associated sources
 
-pmids_union = list(set().union(*(pmids_by_source.values())))
-sources = [list() for i in range(len(pmids_union))]
-# For each PMID ressource in the union set, determine which are the orginals sources of the association.
-for source in pmids_by_source.keys():
-    a = np.array(np.isin(pmids_union, pmids_by_source[source])).nonzero()
-    [sources[index].append(source) for index in a[0].tolist()]
-
-
-
-
-cids_pmids_list.append(Pccompound(cid = cid, pmids = pmids_union, pmids_sources = sources))
-
-new_Ensemble_pccompound = Ensemble_pccompound(cids_pmids_list)
-new_Ensemble_pccompound.export_cids_pmids_triples_ttl("test.txt")
+new_Ensemble_pccompound = Ensemble_pccompound()
+new_Ensemble_pccompound.append_pccompound("25203768", query_builder)
+new_Ensemble_pccompound.append_pccompound("11355423", query_builder)
+new_Ensemble_pccompound.export_cids_pmids_triples_ttl("test2.txt")
