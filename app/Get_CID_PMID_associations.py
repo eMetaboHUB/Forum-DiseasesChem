b7ab9372efa0165c0466b48212df6f806951517b
Maxime Delmas <maxime.delmas@inra.fr>
2020-02-27 16:27:48 +0100

    comment useless code

--- app/Get_CID_PMID_associations.py
+++ app/Get_CID_PMID_associations.py
@@ -9,24 +9,23 @@ from Ensemble_citation import Ensemble_citation
 
 
 # Now we must create a function that takes this union list are try to write all PubMed citation associatied elements
-
-def fetch_mesh(pmids_list, pubmed_citation_folder):
-    """A function to fetch MeSh terms associated to PMID for a PMID list and using a folder that contains all the PubMed XML files
-    - pmids_list: a list of PMIDs
-    - pubmed_citation_folder: a path to the folder that contains all the pubmed XML files"""
-    # List all files in folder
-    xml_citation_files = os.listdir(pubmed_citation_folder)
-    n_file = len(xml_citation_files)
-    # intialiaze lis_Ensemble_citation
-    list_Ensemble_citation = [Ensemble_citation() for i in range(n_file)]
-    # For each file, send path to folder + file name to the list_Ensemble_citation object. 
-    index = 0
-    while ((index < n_file) or (len(pmids_list) == 0)):
-        list_Ensemble_citation[index].open_xml((pubmed_citation_folder + xml_citation_files[index]))
-        print("Exploring file: " + list_Ensemble_citation[index].input_file + "...")
-
-        # Passing to next file
-        index += 1
+# Fonction qui va surement servir a rien .. lol mdr 
+# def fetch_mesh(pmids_list, pubmed_citation_folder):
+#    """A function to fetch MeSh terms associated to PMID for a PMID list and using a folder that contains all the PubMed XML files
+#    - pmids_list: a list of PMIDs
+#    - pubmed_citation_folder: a path to the folder that contains all the pubmed XML files"""
+#    # List all files in folder
+#    xml_citation_files = os.listdir(pubmed_citation_folder)
+#    n_file = len(xml_citation_files)
+#    # intialiaze lis_Ensemble_citation
+#    list_Ensemble_citation = [Ensemble_citation() for i in range(n_file)]
+#    # For each file, send path to folder + file name to the list_Ensemble_citation object. 
+#    index = 0
+#    while ((index < n_file) or (len(pmids_list) == 0)):
+#        list_Ensemble_citation[index].open_xml((pubmed_citation_folder + xml_citation_files[index]))
+#        print("Exploring file: " + list_Ensemble_citation[index].input_file + "...")
+#        # Passing to next file
+#        index += 1
 
 apiKey = "0ddb3479f5079f21272578dc6e040278a508"
 # Building requests
@@ -37,6 +36,7 @@ query_builder = eutils.QueryService(cache = False,
 new_Ensemble_pccompound = Ensemble_pccompound()
 new_Ensemble_pccompound.append_pccompound("25203768", query_builder)
 new_Ensemble_pccompound.append_pccompound("6036", query_builder)
+new_Ensemble_pccompound.append_pccompound("11355423", query_builder)
 # new_Ensemble_pccompound.append_pccompound("11355423", query_builder)
 new_Ensemble_pccompound.export_cids_pmids_triples_ttl("test3.txt")
 
@@ -45,8 +45,8 @@ new_Ensemble_pccompound.export_cid_pmid_endpoint("test_endpoint.txt")
 test = new_Ensemble_pccompound.get_all_pmids()
 
 # fetch_mesh(test, "data/PubMed_MEDLINE/")
-ensbl_cit_test = Ensemble_citation()
-ensbl_cit_test.open_xml("data/PubMed_MEDLINE/pubmed20n0001.xml.gz")
-print(len(test))
-a = ensbl_cit_test.extract_pmids(test)
-print(len(test))
+# ensbl_cit_test = Ensemble_citation()
+# ensbl_cit_test.open_xml("data/PubMed_MEDLINE/pubmed20n0001.xml.gz")
+# print(len(test))
+# a = ensbl_cit_test.extract_pmids(test)
+# print(len(test))
