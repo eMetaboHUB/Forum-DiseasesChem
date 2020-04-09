157d9a30ec91f03387c8a28e7a4c9c813a014192
Maxime Delmas <maxime.delmas@inra.fr>
2020-04-09 12:29:28 +0200

    get pmid by pack

--- app/Ensemble_pccompound.py
+++ app/Ensemble_pccompound.py
@@ -22,36 +22,39 @@ class Ensemble_pccompound:
         self.ressource_version = None
         self.ressource_version_endpoint = None
     
-    def append_pccompound(self, cid, query_builder):
+    def append_pccompound(self, cid_pack, query_builder):
         """This function append a new Pccompound to the pccompound_list attribute. Using the cid, this function send a request to NCBI server via Eutils to get PMID association
-        - cid: a PubChem Compound Identifier 
+        - cid: a list PubChem Compound Identifier 
         - query_builder: a eutils.QueryService object parameterized with cache, retmax, retmode, usehistory and especially the api_key"""
-        pmids_by_source = {}
         # Get CID associated PMID. using try we test if request fail or not. If request fail, it's added to append_failure list
         try:
-            response = query_builder.elink({"dbfrom": "pccompound", "db": "pubmed", "id": cid})
+            response = query_builder.elink({"dbfrom": "pccompound", "db": "pubmed", "id": cid_pack})
         except eutils.EutilsError as fail_request:
-            print("Request on Eutils for compound %s has failed during process, with error name: %s \n -- Compound cid is added to append_failure list" % (cid, fail_request))
-            self.append_failure.append(cid)
+            print("Request on Eutils for current compound pack has failed during process, with error name: %s \n -- Compound cids is added to append_failure list" % (fail_request))
+            self.append_failure.extend(cid_pack)
             return None
         
         root = ET.fromstring(response)
         # Exploring sets
-        for pmid_set in root.findall("./LinkSet/LinkSetDb"):
-            # Each source is assigned as a Key value and PMID list as values
-            pmids_by_source[(pmid_set.find("./LinkName").text)] = [set.text for set in pmid_set.findall("./Link/Id")]
-        # If no refenreces can be found for the cid, exit function and add it to append_failure list
-        if len(pmids_by_source) == 0:
-            self.append_failure.append(cid)
-            return None
-        # Create Union and prepare associated sources
-        pmids_union = list(set().union(*(pmids_by_source.values())))
-        sources = [list() for i in range(len(pmids_union))]
-        # For each PMID ressource in the union set, determine which are the orginals sources of the association.
-        for source in pmids_by_source.keys():
-            a = numpy.array(numpy.isin(pmids_union, pmids_by_source[source])).nonzero()
-            [sources[index].append((source)) for index in a[0].tolist()]
-        self.pccompound_list.append(Pccompound(cid = cid, pmids = pmids_union, pmids_sources = sources))
+        for cid_Element in root.findall("./LinkSet"):
+            # For each LinkSet, get the associated cid :
+            cid = cid_Element.find("./IdList/Id").text
+            pmids_by_source = {}
+            for pmid_set in cid_Element.findall("./LinkSetDb"):
+                # Each source is assigned as a Key value and PMID list as values
+                pmids_by_source[(pmid_set.find("./LinkName").text)] = [set.text for set in pmid_set.findall("./Link/Id")]
+            # If no refenreces can be found for the cid, exit function and add it to append_failure list
+            if len(pmids_by_source) == 0:
+                self.append_failure.append(cid)
+                continue
+            # Create Union and prepare associated sources
+            pmids_union = list(set().union(*(pmids_by_source.values())))
+            sources = [list() for i in range(len(pmids_union))]
+            # For each PMID ressource in the union set, determine which are the orginals sources of the association.
+            for source in pmids_by_source.keys():
+                a = numpy.array(numpy.isin(pmids_union, pmids_by_source[source])).nonzero()
+                [sources[index].append((source)) for index in a[0].tolist()]
+            self.pccompound_list.append(Pccompound(cid = cid, pmids = pmids_union, pmids_sources = sources))
     
     def fill_cids_pmids_graph(self, g, namespaces_dict):
         """This function create a rdflib graph containing all the cid - pmid associations contains in the Ensemble_pccompound object.
