7ed42355107e61a96ccae6d84e66e2131a29ce61
Maxime Delmas <maxime.delmas@inra.fr>
2020-04-30 18:37:54 +0200

    metab2mesh close to be done

---
+++ app/metab2mesh/post_process_metab2mesh.R
@@ -0,0 +1,49 @@
+library(optparse)
+library(tidyverse)
+option_list <- list(
+  make_option(c("-i", "--p_metab2mesh"), type="character", default=NULL,
+              help="path to metab2mesh fisher table", metavar="character"),
+  make_option(c("-c", "--p_CID_labels"), type="character", default=NULL,
+              help="path to CID labels table", metavar="character"),
+  make_option(c("-m", "--p_MESH_labels"), type="character", default=NULL,
+              help="path to MESH labels table", metavar="character"),
+  make_option(c("-c", "--p_MESH_InchI"), type="character", default=NULL,
+              help="path to CID InchI table", metavar="character")
+);
+
+opt_parser <- OptionParser(option_list=option_list);
+opt <- parse_args(opt_parser);
+
+# Prepare files :
+path_metab2mesh <- opt$p_metab2mesh
+path_CID_labels <- opt$p_CID_labels
+path_MESH_label <- opt$p_MESH_labels
+path_CID_InchI <- opt$p_MESH_InchI
+
+# path_metab2mesh = "~/Documents/Thèse/building_database/FORUM/metdiseasedatabase/data/metab2mesh/metab2mesh_fisher/metab2mesh_fisher.csv"
+# path_CID_labels = "~/Documents/Thèse/building_database/FORUM/metdiseasedatabase/data/metab2mesh/CID_LABEL/res_full.txt"
+# path_MESH_label = "~/Documents/Thèse/building_database/FORUM/metdiseasedatabase/data/metab2mesh/MESH_LABEL/res_full.csv"
+# path_CID_InchI = "~/Documents/Thèse/building_database/FORUM/metdiseasedatabase/data/metab2mesh/CID_INCHI/res_full.txt"
+# Add columns names and adjusted p.value
+metab2mesh <- read.table(path_metab2mesh, sep = ",", stringsAsFactors = FALSE)
+colnames(metab2mesh) <- c("CID", "MESH", "COOC", "TOTAL_PMID_CID", "TOTAL_PMID_MESH", "TOTAL_PMID", "p.value", "odds_ratio", "Fold_change", "Chisq_stat")
+metab2mesh$p.adj <- p.adjust(metab2mesh$p.value, method = "BH")
+
+# Parse CID labels
+cid_labels <- read.table(path_CID_labels, sep = "\t", stringsAsFactors = FALSE, quote = '')
+colnames(cid_labels) <- c("CID", "CID_LABEL")
+
+# Parse MeSH labels
+mesh_labels <- read.table(path_MESH_label, sep = ",", stringsAsFactors = FALSE)
+colnames(mesh_labels) <- c("MESH", "MESH_LABEL")
+
+# Parse CID InchI
+cid_inchi <- read.table(path_CID_InchI, sep = "\t", stringsAsFactors = FALSE, quote = '')
+colnames(cid_inchi) <- c("CID", "CID_INCHI")
+
+# Add CID labels to metab2mesh :
+metab2mesh <- metab2mesh %>% left_join(cid_labels, by = "CID")
+metab2mesh <- metab2mesh %>% left_join(mesh_labels, by = "MESH")
+metab2mesh <- metab2mesh %>% left_join(cid_inchi, by = "CID")
+
+
--- app/metab2mesh/request_virtuoso.py
+++ app/metab2mesh/request_virtuoso.py
@@ -20,11 +20,12 @@ prefix = """
     PREFIX meshv: <http://id.nlm.nih.gov/mesh/vocab#>
     PREFIX mesh: <http://id.nlm.nih.gov/mesh/>
     PREFIX voc: <http://myorg.com/voc/doc#>
-    prefix cito: <http://purl.org/spar/cito/>
-    prefix fabio:	<http://purl.org/spar/fabio/> 
-    prefix owl: <http://www.w3.org/2002/07/owl#> 
-    prefix void: <http://rdfs.org/ns/void#>
-    prefix cid:   <http://rdf.ncbi.nlm.nih.gov/pubchem/compound/>
+    PREFIX cito: <http://purl.org/spar/cito/>
+    PREFIX fabio:	<http://purl.org/spar/fabio/> 
+    PREFIX owl: <http://www.w3.org/2002/07/owl#> 
+    PREFIX void: <http://rdfs.org/ns/void#>
+    PREFIX cid:   <http://rdf.ncbi.nlm.nih.gov/pubchem/compound/>
+    PREFIX sio: <http://semanticscience.org/resource/>
 """
 
 distinct_pmid_by_CID = """
@@ -160,6 +161,77 @@ limit %d
 offset %d
 """
 
+
+list_of_distinct_pmid_by_CID_MeSH = """
+select ?id (group_concat(distinct ?str_pmid;separator=\";\") as ?list_pmid)
+where
+{
+    {
+        select (concat(strafter(STR(?cid),\"http://rdf.ncbi.nlm.nih.gov/pubchem/compound/CID\"), \"_\", strafter(STR(?mesh),\"http://id.nlm.nih.gov/mesh/\")) as ?id) (strafter(str(?pmid), \"http://rdf.ncbi.nlm.nih.gov/pubchem/reference/PMID\") as ?str_pmid)
+        where {
+            {
+                select ?cid where {
+                    {
+                        select distinct ?cid where {
+                            ?cid cito:isDiscussedBy ?pmid .
+                        }
+                        order by ?cid
+                    }
+                }
+                limit %d
+                offset %d
+            }
+            ?cid cito:isDiscussedBy ?pmid .
+            ?pmid fabio:hasSubjectTerm|fabio:hasSubjectTerm/meshv:hasDescriptor ?mesh .
+            ?mesh a meshv:TopicalDescriptor .
+            ?mesh meshv:treeNumber ?tn .
+            FILTER(REGEX(?tn,\"(C|A|D|G|B|F|I|J)\"))
+        }
+        group by ?cid ?mesh
+        order by ?id
+    } 
+}
+limit %d
+offset %d
+"""
+
+
+MESH_name = """
+select ?MESH str(?str_f_label)
+where
+{
+    {
+        select (strafter(STR(?mesh),\"http://id.nlm.nih.gov/mesh/\") as ?MESH) MIN(str(?label)) as ?str_f_label
+        where
+        {
+            {
+                select ?mesh
+                where
+                {
+                    {
+                        select distinct ?mesh
+                        where
+                        {
+                            ?mesh a meshv:TopicalDescriptor .
+                            ?mesh meshv:treeNumber ?tn .
+                            FILTER(REGEX(?tn,\"(C|A|D|G|B|F|I|J)\"))
+                        }
+                        order by ?mesh
+                    }
+                }
+                limit %d
+                offset %d
+            }
+        ?mesh rdfs:label ?label
+        }
+        group by ?mesh
+        order by ?mesh
+    }
+}
+limit %d
+offset %d
+"""
+
 url = "http://localhost:9980/sparql/"
 
 
@@ -253,7 +325,7 @@ def prepare_data_frame(path_to_CID_MESH, path_to_CID_PMID, path_to_MESH_PMID, nb
     data["TOTAL_PMID"] = nbTotal_PMID
     df_size=len(data)
     for i, start in enumerate(range(0, df_size, file_size)):
-        data[start:start+file_size].to_csv(out_path + 'metab2mesh_{}.csv'.format(i))
+        data[start:start+file_size].to_csv(out_path + 'metab2mesh_{}.csv'.format(i), index = False)
     return data
 
 
@@ -266,18 +338,6 @@ count_cid_query = prefix + """
     }
 """
 
-# For CID - distinct PMID, when request by pack, some CID may not have linked publication for which there is a mesh from the selected fields. So the number of returned CID may be less than the pack size
-# For MeSH - distinct PMID, when request by pack, some MeSH selected from their fields (in the total MESH thesaurus) may not have linked publication for which there is CID. So the number of returned MeSH may be less than the pack size.
-
-data["query"] = count_cid_query
-count_cid_res = requests.post(url = url, headers = header, data = data)
-count_cid = int(count_cid_res.text.splitlines().pop(1))
-
-# Get CID - MeSH coocurences :
-parallelize_query_by_offset(count_cid, distinct_pmid_by_CID_MeSH, prefix, header, data, url, 1000, 1000000, "data/metab2mesh/CID_MESH/", 8)
-# Get CID distinct PMID :
-parallelize_query_by_offset(count_cid, distinct_pmid_by_CID, prefix, header, data, url, 10000, 10000, "data/metab2mesh/CID_PMID/", 8)
-
 # Get By MeSH
 count_mesh_query = prefix + """
     select (count(distinct ?mesh) as ?count_MESH) 
@@ -288,10 +348,24 @@ count_mesh_query = prefix + """
         FILTER(REGEX(?tn,\"(C|A|D|G|B|F|I|J)\"))
     }
 """
+
+# For CID - distinct PMID, when request by pack, some CID may not have linked publication for which there is a mesh from the selected fields. So the number of returned CID may be less than the pack size
+# For MeSH - distinct PMID, when request by pack, some MeSH selected from their fields (in the total MESH thesaurus) may not have linked publication for which there is CID. So the number of returned MeSH may be less than the pack size.
+
+# Count distinct CID
+data["query"] = count_cid_query
+count_cid_res = requests.post(url = url, headers = header, data = data)
+count_cid = int(count_cid_res.text.splitlines().pop(1))
+
+# Get CID - MeSH coocurences :
+parallelize_query_by_offset(count_cid, distinct_pmid_by_CID_MeSH, prefix, header, data, url, 1000, 1000000, "data/metab2mesh/CID_MESH/", 8)
+# Get CID distinct PMID :
+parallelize_query_by_offset(count_cid, distinct_pmid_by_CID, prefix, header, data, url, 10000, 10000, "data/metab2mesh/CID_PMID/", 8)
+
+# Count distinct MESH
 data["query"] = count_mesh_query
 count_mesh_res = requests.post(url = url, headers = header, data = data)
 count_mesh = int(count_mesh_res.text.splitlines().pop(1))
-
 # Get MeSH distinct PMID :
 parallelize_query_by_offset(count_mesh, distinct_pmid_by_MeSH, prefix, header, data, url, 3000, 3000, "data/metab2mesh/MESH_PMID/", 8)
 
@@ -299,6 +373,17 @@ parallelize_query_by_offset(count_mesh, distinct_pmid_by_MeSH, prefix, header, d
 data["query"] = prefix + distinct_all_pmids
 count_pmids_res = requests.post(url = url, headers = header, data = data)
 count_pmids = int(count_pmids_res.text.splitlines().pop(1))
+# Nb. total pmids = 8754160
 
-df_metab2mesh = prepare_data_frame("data/metab2mesh/CID_MESH/res_full.csv", "data/metab2mesh/CID_PMID/res_full.csv", "data/metab2mesh/MESH_PMID/res_full.csv", count_pmids, "data/metab2mesh/res/", 10000)
+# Extract MeSH Names
+parallelize_query_by_offset(count_mesh, MESH_name, prefix, header, data, url, 3000, 30000, "data/metab2mesh/MESH_LABEL/", 8)
+# To Extract CID Names, please use the PubChem translation service at https://pubchem.ncbi.nlm.nih.gov/idexchange/idexchange.cgi using a list of all cids (ex: all_linked_ids)
+# The same thing was done for CID
 
+# Extract list of pmids associated to a CID - MeSH co-occurence, separated by ";". WARNING: In the request, the group concat term must be in the last select because of large string can't be handeling by sub-queries ans link to a superior select. 
+# However, to handle offset with large "order by", the order by term must be placed in a sub query, before output all results, but the group_concat muste be in the firs select so ... problem !
+# So the order provided in this query is the same as in the others
+parallelize_query_by_offset(count_cid, list_of_distinct_pmid_by_CID_MeSH, prefix, header, data, url, 1000, 1000000, "data/metab2mesh/CID_MESH_PMID_LISt/", 8)
+
+
+df_metab2mesh = prepare_data_frame("data/metab2mesh/CID_MESH/res_full.csv", "data/metab2mesh/CID_PMID/res_full.csv", "data/metab2mesh/MESH_PMID/res_full.csv", count_pmids, "data/metab2mesh/res2/", 30000)
