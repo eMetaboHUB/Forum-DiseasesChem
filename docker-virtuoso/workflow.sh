625af8977655f0274bd776ef7757eb03c543b5fd
Maxime Delmas <maxime.delmas@inra.fr>
2020-05-20 13:13:29 +0200

    every things seems to works

--- docker-virtuoso/workflow.sh
+++ docker-virtuoso/workflow.sh
@@ -43,7 +43,7 @@ echo "DELETE FROM DB.DBA.RDF_QUAD ;" >> share/upload.sh
 
 # echo "ld_dir_all ('./dumps/HumanGEM/', '*.ttl', 'http://database/ressourcesSMBL');" >> share/upload.sh
 
-# echo "ld_dir_all ('./dumps/MetaNetX/', '*.ttl.gz', 'http://database/MetaNetX');" >> share/upload.sh
+echo "ld_dir_all ('./dumps/MetaNetX/', '*.ttl.gz', 'http://database/MetaNetX');" >> share/upload.sh
 
 # echo "ld_dir_all ('./dumps/annot_graphs/MetaNetX_BiGG/', '*.ttl', 'http://database/ressources');" >> share/upload.sh
 # echo "ld_dir_all ('./dumps/annot_graphs/MetaNetX_BiGG/', '*.trig', '');" >> share/upload.sh
@@ -68,26 +68,26 @@ echo "DELETE FROM DB.DBA.RDF_QUAD ;" >> share/upload.sh
 # echo "ld_dir_all ('./dumps/CID_PMID_endpoints/SMBL_2020-04-06/', '*.ttl', 'http://database/ressources');" >> share/upload.sh
 # echo "ld_dir_all ('./dumps/CID_PMID_endpoints/SMBL_2020-04-06/', '*.trig', '');" >> share/upload.sh
 
-echo "ld_dir_all ('./dumps/PMID_CID/2020-04-18/', '*.ttl', 'http://database/ressources');" >> share/upload.sh
-echo "ld_dir_all ('./dumps/PMID_CID/2020-04-18/', '*.trig.gz', '');" >> share/upload.sh
+# echo "ld_dir_all ('./dumps/PMID_CID/2020-04-18/', '*.ttl', 'http://database/ressources');" >> share/upload.sh
+# echo "ld_dir_all ('./dumps/PMID_CID/2020-04-18/', '*.trig.gz', '');" >> share/upload.sh
 
 # echo "ld_dir_all ('./dumps/PMID_CID_endpoints/2020-04-18/', '*.ttl', 'http://database/ressources');" >> share/upload.sh
 # echo "ld_dir_all ('./dumps/PMID_CID_endpoints/2020-04-18/', '*.trig.gz', '');" >> share/upload.sh
 
-# echo "ld_dir_all ('./dumps/PubChem_Compound/CompoundFiltered/SBML_2020-05-07/', '*.ttl', 'http://database/ressources');" >> share/upload.sh
-# echo "ld_dir_all ('./dumps/PubChem_Compound/CompoundFiltered/SBML_2020-05-07/', '*.trig', '');" >> share/upload.sh
+echo "ld_dir_all ('./dumps/PubChem_Compound/CompoundFiltered/SBML_2020-05-07/', '*.ttl', 'http://database/ressources');" >> share/upload.sh
+echo "ld_dir_all ('./dumps/PubChem_Compound/CompoundFiltered/SBML_2020-05-07/', '*.trig', '');" >> share/upload.sh
 
-# echo "ld_dir_all ('./dumps/PubChem_Descriptor/DescriptorFiltered/SBML_2020-05-07/', '*.ttl', 'http://database/ressources');" >> share/upload.sh
-# echo "ld_dir_all ('./dumps/PubChem_Descriptor/DescriptorFiltered/SBML_2020-05-07/', '*.trig', '');" >> share/upload.sh
+echo "ld_dir_all ('./dumps/PubChem_Descriptor/DescriptorFiltered/SBML_2020-05-07/', '*.ttl', 'http://database/ressources');" >> share/upload.sh
+echo "ld_dir_all ('./dumps/PubChem_Descriptor/DescriptorFiltered/SBML_2020-05-07/', '*.trig', '');" >> share/upload.sh
 
-echo "ld_dir_all ('./dumps/PubChem_References/PrimarySubjectTermFiltered/2020-04-20/', '*.ttl', 'http://database/ressources');" >> share/upload.sh
-echo "ld_dir_all ('./dumps/PubChem_References/PrimarySubjectTermFiltered/2020-04-20/', '*.trig.gz', '');" >> share/upload.sh
+# echo "ld_dir_all ('./dumps/PubChem_References/PrimarySubjectTermFiltered/2020-04-20/', '*.ttl', 'http://database/ressources');" >> share/upload.sh
+# echo "ld_dir_all ('./dumps/PubChem_References/PrimarySubjectTermFiltered/2020-04-20/', '*.trig.gz', '');" >> share/upload.sh
 
-echo "ld_dir_all ('./dumps/PubChem_References/referenceFiltered/2020-04-19/', '*.ttl', 'http://database/ressources');" >> share/upload.sh
-echo "ld_dir_all ('./dumps/PubChem_References/referenceFiltered/2020-04-19/', '*.trig.gz', '');" >> share/upload.sh
+# echo "ld_dir_all ('./dumps/PubChem_References/referenceFiltered/2020-04-19/', '*.ttl', 'http://database/ressources');" >> share/upload.sh
+# echo "ld_dir_all ('./dumps/PubChem_References/referenceFiltered/2020-04-19/', '*.trig.gz', '');" >> share/upload.sh
 
-echo "ld_dir_all ('./dumps/MeSH/', '*.ttl', 'http://database/ressources');" >> share/upload.sh
-echo "ld_dir_all ('./dumps/MeSH/', '*.trig', '');" >> share/upload.sh
+# echo "ld_dir_all ('./dumps/MeSH/', '*.ttl', 'http://database/ressources');" >> share/upload.sh
+# echo "ld_dir_all ('./dumps/MeSH/', '*.trig', '');" >> share/upload.sh
 
 echo "ld_dir_all ('./dumps/vocabulary/', '*.ttl', 'http://database/inference-rules/');" >> share/upload.sh
 echo "ld_dir_all ('./dumps/vocabulary/', '*.rdf', 'http://database/inference-rules/');" >> share/upload.sh
