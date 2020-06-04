a08ea2e1596e7b14fa345f57f8824939d11d4c41
Maxime Delmas <maxime.delmas@inra.fr>
2020-06-04 19:16:16 +0200

    add chebi_onto_mesh

--- app/metab2mesh/post-processes/post_process_metab2mesh.R
+++ app/metab2mesh/post-processes/post_process_metab2mesh.R
@@ -23,10 +23,6 @@ path_MESH_label <- opt$p_MESH_labels
 path_CID_InchI <- opt$p_CID_InchI
 path_out <- opt$p_out
 
-# path_metab2mesh = "~/Documents/Thèse/building_database/FORUM/metdiseasedatabase/data/metab2mesh/metab2mesh_fisher/metab2mesh_fisher.csv"
-# path_CID_labels = "~/Documents/Thèse/building_database/FORUM/metdiseasedatabase/data/metab2mesh/CID_LABEL/res_full.txt"
-# path_MESH_label = "~/Documents/Thèse/building_database/FORUM/metdiseasedatabase/data/metab2mesh/MESH_LABEL/res_full.csv"
-# path_CID_InchI = "~/Documents/Thèse/building_database/FORUM/metdiseasedatabase/data/metab2mesh/CID_INCHI/res_full.txt"
 
 # Add columns names and adjusted p.value
 metab2mesh <- read.table(path_metab2mesh, sep = ",", stringsAsFactors = FALSE)
@@ -34,20 +30,20 @@ colnames(metab2mesh) <- c("CID", "MESH", "COOC", "TOTAL_PMID_CID", "TOTAL_PMID_M
 metab2mesh$p.adj <- p.adjust(metab2mesh$p.value, method = "BH")
 
 # Parse CID labels
-cid_labels <- read.table(path_CID_labels, sep = "\t", stringsAsFactors = FALSE, quote = '')
-colnames(cid_labels) <- c("CID", "CID_LABEL")
+# cid_labels <- read.table(path_CID_labels, sep = "\t", stringsAsFactors = FALSE, quote = '')
+# colnames(cid_labels) <- c("CID", "CID_LABEL")
 
 # Parse MeSH labels
-mesh_labels <- read.table(path_MESH_label, sep = ",", stringsAsFactors = FALSE)
-colnames(mesh_labels) <- c("MESH", "MESH_LABEL")
+# mesh_labels <- read.table(path_MESH_label, sep = ",", stringsAsFactors = FALSE)
+# colnames(mesh_labels) <- c("MESH", "MESH_LABEL")
 
 # Parse CID InchI
-cid_inchi <- read.table(path_CID_InchI, sep = "\t", stringsAsFactors = FALSE, quote = '')
-colnames(cid_inchi) <- c("CID", "CID_INCHI")
+# cid_inchi <- read.table(path_CID_InchI, sep = "\t", stringsAsFactors = FALSE, quote = '')
+# colnames(cid_inchi) <- c("CID", "CID_INCHI")
 
 # Add CID labels to metab2mesh :
-metab2mesh <- metab2mesh %>% left_join(cid_labels, by = "CID")
-metab2mesh <- metab2mesh %>% left_join(mesh_labels, by = "MESH")
-metab2mesh <- metab2mesh %>% left_join(cid_inchi, by = "CID")
+# metab2mesh <- metab2mesh %>% left_join(cid_labels, by = "CID")
+# metab2mesh <- metab2mesh %>% left_join(mesh_labels, by = "MESH")
+# metab2mesh <- metab2mesh %>% left_join(cid_inchi, by = "CID")
 
 write.table(metab2mesh, file = path_out, sep = ",", row.names = FALSE, col.names = TRUE)
