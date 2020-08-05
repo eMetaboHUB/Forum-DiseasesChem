1060999eb175fcc2ab66fda589d666505e6c4fad
Maxime Delmas <maxime.delmas@inra.fr>
2020-08-05 16:57:29 +0200

    add for ChEBI

--- app/metab2mesh/SPARQL/requests_wordcloud.py
+++ app/metab2mesh/SPARQL/requests_wordcloud.py
@@ -49,3 +49,40 @@ PubChem = """
         ?mesh rdfs:label ?label .
     }
 """
+
+# (strafter(STR(?chebi),\"http://purl.obolibrary.org/obo/CHEBI_\") as ?CHEBI) (strafter(STR(?mesh),\"http://id.nlm.nih.gov/mesh/\") as ?MESH) (count(distinct ?pmid) as ?count) 
+
+ChEBI = """
+    select ?label ?count
+    %s
+    where
+    {
+        {
+            select ?mesh (count(distinct ?pmid) as ?count)
+            where
+            {
+                {
+                    select ?pmid
+                    where
+                    {        
+                        ?cid a+ %s .
+                        ?cid cito:isDiscussedBy ?pmid .
+                        ?pmid (fabio:hasSubjectTerm|fabio:hasSubjectTerm/meshv:hasDescriptor) ?mesh_ini .
+                        ?mesh_ini a meshv:TopicalDescriptor .
+                        ?mesh_ini meshv:active 1 .
+                        ?mesh_ini (meshv:treeNumber|meshv:treeNumber/meshv:parentTreeNumber+) ?tn .
+                        %s meshv:treeNumber ?tn .
+                    }
+                    group by ?chebi ?mesh
+                }
+                ?pmid (fabio:hasSubjectTerm|fabio:hasSubjectTerm/meshv:hasDescriptor) ?mesh .
+                ?mesh a meshv:TopicalDescriptor .
+                ?mesh meshv:active 1 .
+                ?mesh meshv:treeNumber ?tn .
+                FILTER(REGEX(?tn,\"(%s)\")) .
+            }
+            group by ?mesh
+        }
+        ?mesh rdfs:label ?label .
+    }
+"""
