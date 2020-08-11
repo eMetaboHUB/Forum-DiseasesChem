48a9260c4b8737860302b39d55a7b8c4375c5cb6
Maxime Delmas <maxime.delmas@inra.fr>
2020-08-11 15:52:49 +0200

    fetching CHEMONT ids seems to work

--- app/ChemOnt/fetch_ChemOnt.py
+++ app/ChemOnt/fetch_ChemOnt.py
@@ -3,6 +3,7 @@ import sys
 import pandas as pd
 import numpy as np
 import multiprocessing as mp
+import subprocess
 sys.path.insert(1, 'app/')
 from Database_ressource_version import Database_ressource_version
 from processing_functions import *
@@ -56,15 +57,14 @@ with open("ids_no_classify.log", "w") as f_log:
 # Read input file
 CID_inchiKeys = pd.read_csv(config["INPUT"].get("path"), sep = "\t", dtype= {'CID': str, 'INCHIKEY':str})
 df_list = np.array_split(CID_inchiKeys, n_processes)
-print(df_list)
 
 # On initialise les ressources :
 ClassyFire_direct_p = Database_ressource_version(ressource = "ClassyFire/direct-parent", version = version)
-ClassyFire_alternative_p = Database_ressource_version(ressource = "ClassyFire/alternatives-parents", version = version)
+ClassyFire_alternative_p = Database_ressource_version(ressource = "ClassyFire/alternative-parents", version = version)
 
 # On initialise les listes de graphs :
 ClassyFire_direct_p_graph_l = [ClassyFire_direct_p.create_data_graph(["compound", "classyfire"], namespaces) for i in range(0, n_processes)]
-ClassyFire_alternative_p_graph_l = [ClassyFire_direct_p.create_data_graph(["compound", "classyfire"], namespaces) for i in range(0, n_processes)]
+ClassyFire_alternative_p_graph_l = [ClassyFire_alternative_p.create_data_graph(["compound", "classyfire"], namespaces) for i in range(0, n_processes)]
 
 # On initialise les path où exporter les graphs :
 path_direct_p = outpath + "/" + ClassyFire_direct_p.ressource + "/" + ClassyFire_direct_p.version + "/"
@@ -77,9 +77,30 @@ if not os.path.exists(path_alternative_p):
 
 pool = mp.Pool(processes = n_processes, maxtasksperchild = 20)
 results = [pool.apply_async(classify_df, args=(i, df_list[i], ClassyFire_direct_p_graph_l[i], ClassyFire_alternative_p_graph_l[i], path_direct_p, path_alternative_p)) for i in range(0, n_processes)]
-output = [p.get() for p in results]
+graph_sizes = [p.get() for p in results]
 # Close Pool
 pool.close()
 pool.join()
+export_ressource_metatdata(ClassyFire_alternative_p, ClassyFire_direct_p, graph_sizes, [rdflib.URIRef("http://database/ressources/PubChem/compound"), rdflib.URIRef("http://database/ressources/ChemOnt")], path_direct_p, path_alternative_p)
 
+# Compress files:
 
+try:
+    subprocess.run("gzip " + outpath + "/ClassyFire/direct-parent/" + version + "/*.trig", shell = True, check=True, stderr = subprocess.PIPE)
+    subprocess.run("gzip " + outpath + "/ClassyFire/alternative-parents/" + version + "/*.trig", shell = True, check=True, stderr = subprocess.PIPE)
+except subprocess.CalledProcessError as e:
+    print("Error while trying to compress files")
+    print(e)
+    sys.exit(3)
+
+# Write ouput file header :
+with open(outpath + "/upload_ClassyFire.sh", "w") as upload_f:
+    upload_f.write("delete from DB.DBA.load_list ;\n")
+    upload_f.write("ld_dir_all ('./dumps/ClassyFire/direct-parent/" + version + "/', '*.trig.gz', '');\n")
+    upload_f.write("ld_dir_all ('./dumps/ClassyFire/direct-parent/" + version + "/', 'void.ttl', '" + str(ClassyFire_direct_p.uri_version) + "');\n")
+    upload_f.write("ld_dir_all ('./dumps/ClassyFire/alternative-parents/" + version + "/', '*.trig.gz', '');\n")
+    upload_f.write("ld_dir_all ('./dumps/ClassyFire/alternative-parents/" + version + "/', 'void.ttl', '" + str(ClassyFire_alternative_p.uri_version) + "');\n")
+    upload_f.write("select * from DB.DBA.load_list;\n")
+    upload_f.write("rdf_loader_run();\n")
+    upload_f.write("checkpoint;\n")
+    upload_f.write("select * from DB.DBA.LOAD_LIST where ll_error IS NOT NULL;\n")
--- app/ChemOnt/processing_functions.py
+++ app/ChemOnt/processing_functions.py
@@ -2,7 +2,7 @@ import requests
 import json
 import time
 import rdflib
-from rdflib.namespace import RDF
+from rdflib.namespace import RDF, VOID, DCTERMS, XSD
 
 
 def classify_df(df_index, df, g_direct_parent, g_alternative_parent, path_direct_p, path_alternative_p):
@@ -15,6 +15,7 @@ def classify_df(df_index, df, g_direct_parent, g_alternative_parent, path_direct
     print("Serialyze graphs")
     g_direct_parent.serialize(destination=path_direct_p + "classyfire_direct_parent_" + str(df_index + 1) + ".trig", format='trig')
     g_alternative_parent.serialize(destination=path_alternative_p + "classyfire_alternative_parent_" + str(df_index + 1) + ".trig", format='trig')
+    return [len(g_direct_parent), len(set([str(s) for s in g_direct_parent.subjects()])), len(g_alternative_parent), len(set([str(s) for s in g_alternative_parent.subjects()]))]
 
 def get_entity_from_ClassyFire(CID, InchiKey):
     """
@@ -56,9 +57,32 @@ def parse_entities(classif):
     return(chemont_ids)
 
 def add_triples(CID, chemont_ids, g_direct_parent, g_alternative_parent):
-    print(chemont_ids)
-    print(CID)
     g_direct_parent.add((rdflib.URIRef("http://rdf.ncbi.nlm.nih.gov/pubchem/compound/CID" + CID), RDF["type"], rdflib.URIRef("http://purl.obolibrary.org/obo/CHEMONTID_" + chemont_ids[0])))
     if len(chemont_ids) > 1:
         for alt_p in chemont_ids[1:]:
             g_alternative_parent.add((rdflib.URIRef("http://rdf.ncbi.nlm.nih.gov/pubchem/compound/CID" + CID), RDF["type"], rdflib.URIRef("http://purl.obolibrary.org/obo/CHEMONTID_" + alt_p)))
+
+def export_ressource_metatdata(ClassyFire_direct_p, ClassyFire_alternative_p, graph_sizes, uri_targeted_ressources, path_direct_p, path_alternative_p):
+
+    # On ajoute les infos pour la première ressource:
+    for i in range(1, (len(graph_sizes) + 1)):
+        ClassyFire_direct_p.add_DataDump("classyfire_direct_parent_" + str(i) + ".trig")
+        ClassyFire_alternative_p.add_DataDump("classyfire_alternative_parent_" + str(i) + ".trig")
+    ClassyFire_direct_p.add_version_attribute(RDF["type"], VOID["Linkset"])
+    ClassyFire_alternative_p.add_version_attribute(RDF["type"], VOID["Linkset"])
+    for uri_targeted_ressource in uri_targeted_ressources:
+        ClassyFire_direct_p.add_version_attribute(VOID["target"], uri_targeted_ressource)
+        ClassyFire_alternative_p.add_version_attribute(VOID["target"], uri_targeted_ressource)
+    ClassyFire_direct_p.add_version_attribute(DCTERMS["description"], rdflib.Literal("This subset contains RDF triples providing links between PubChem compounds and their class according to ChemOnt ontology from ClassyFire. The provided class correspond to the \"Direct Parent\", representing the dominant class in the molecule"))
+    ClassyFire_direct_p.add_version_attribute(DCTERMS["title"], rdflib.Literal("ChemOnt Classification - Direct parent"))
+    # On ajoute les infos pour la seconde ressource, les endpoint:
+    ClassyFire_alternative_p.add_version_attribute(DCTERMS["description"], rdflib.Literal("This subset contains RDF triples providing links between PubChem compounds and their classes according to ChemOnt ontology from ClassyFire. The provided classes correspond to the \"Alternative Parents\", representing classes describing the molecule but which not have an ancestor–descendant relationship with each other or with the Direct Parent"))
+    ClassyFire_alternative_p.add_version_attribute(DCTERMS["title"], rdflib.Literal("ChemOnt Classification - Alternative parents"))
+    # On exporte le graph des metadata :
+    print(" Export version graph with metadata ...\n", end = '')
+    ClassyFire_direct_p.add_version_attribute(VOID["triples"], rdflib.Literal(sum([g[0] for g in graph_sizes]), datatype=XSD.long))
+    ClassyFire_direct_p.add_version_attribute(VOID["distinctSubjects"], rdflib.Literal(sum([g[1] for g in graph_sizes]), datatype=XSD.long ))
+    ClassyFire_alternative_p.add_version_attribute(VOID["triples"], rdflib.Literal(sum([g[2] for g in graph_sizes]), datatype=XSD.long ))
+    ClassyFire_alternative_p.add_version_attribute(VOID["distinctSubjects"], rdflib.Literal(sum([g[3] for g in graph_sizes]), datatype=XSD.long ))
+    ClassyFire_direct_p.version_graph.serialize(destination= path_direct_p + "void.ttl", format='turtle')
+    ClassyFire_alternative_p.version_graph.serialize(destination= path_alternative_p + "void.ttl", format='turtle')
