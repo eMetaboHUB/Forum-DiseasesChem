e39281c402b82dd4e39cc444e6ec71e37216d865
ofilangi <olivier.filangi@inrae.fr>
2020-09-22 14:28:43 +0000

    remove docker directory

--- docker-virtuoso/load_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-dockvirtuoso=$(docker-compose ps | grep virtuoso | awk '{print $1}')
-
-sleep 10
-
-docker exec -t $dockvirtuoso bash -c '/usr/local/virtuoso-opensource/bin/isql-v 1111 dba "FORUM-Met-Disease-DB" ./dumps/upload_data.sh'
--- w2_virtuoso.sh
+++ w2_virtuoso.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# USAGE (1) : ${0} start $PYDIO_PASSWD    # start virtuoso and load data (if needed) from PYDIO INRAE FORUM Project
+# necessite ./share/updload.sh
+
+# USAGE (1) : ${0} start                  # start virtuoso and load data with script ./share/updload.sh
 # USAGE (2) : ${0} stop                   # stop virtuoso                  
 # USAGE (3) : ${0} clean                  # remove docker directory
 
@@ -12,12 +14,10 @@ then
 fi
 
 CMD=$1
-PYDIO_PASSWD=$2
 
-# LISTEN_PORT   Port to listen
-# PASSWORD      admin password
-# GRAPH         default graph name (iri)
-# ALLOW_UDPATE  allow sparql update (YES | NO)
+RESOURCES_DIR=./share/
+UPDATA_FILE=update.sh
+
 
 COMPOSE_PROJECT_NAME="metdisease"
 LISTEN_PORT="9980"
@@ -25,29 +25,6 @@ PASSWORD="FORUM-Met-Disease-DB"
 GRAPH="http://default#"
 ALLOW_UDPATE="true"
 
-URL_PYDIO_TTL="https://pfem.clermont.inra.fr/pydio/public/7af464/dl/"
-OPTION_PYDIO_INRAE="-u none:$PYDIO_PASSWD"
-
-RESOURCES_DIR=./share/
-
-function download_resources() {
-    echo " -- Download resources."
-
-    # download files 
-	
-	[ ! -d ${RESOURCES_DIR}/MetaNetX ] && mkdir -p ${RESOURCES_DIR}/MetaNetX/
-	[ ! -f ${RESOURCES_DIR}/MetaNetX/HumanGEM.ttl ] && wget --user none --password ${PYDIO_PASSWD} ${URL_PYDIO_TTL}/HumanGEM.ttl -P ${RESOURCES_DIR}/MetaNetX/
-	[ ! -f ${RESOURCES_DIR}/MetaNetX/metanetx.ttl ] && wget --user none --password ${PYDIO_PASSWD} ${URL_PYDIO_TTL}/metanetx.ttl -P ${RESOURCES_DIR}/MetaNetX/
-	
-	[ ! -d ${RESOURCES_DIR}/vocabulary ] && wget --user none --password ${PYDIO_PASSWD} ${URL_PYDIO_TTL}/vocabulary -O ${RESOURCES_DIR}/vocabulary.zip
-	[ -f ${RESOURCES_DIR}/vocabulary.zip ] && unzip ${RESOURCES_DIR}/vocabulary.zip -d ${RESOURCES_DIR}/ && rm -rf ${RESOURCES_DIR}/vocabulary.zip
-}
-
-## - not usefull actually -
-function build() {
-    echo " -- Build Docker Metdisease."
-	docker build -t forum/metdisease .
-}
 
 function waitStarted() {
     set +e
@@ -114,65 +91,6 @@ services:
 networks:
     ${NETWORK_NAME}:
 EOF
-
-UPDATA_FILE=update.sh
-
-cat << EOF | tee ${RESOURCES_DIR}/${UPDATA_FILE} > /dev/null
-
-GRANT SELECT ON "DB"."DBA"."SPARQL_SINV_2" TO "SPARQL";
-GRANT EXECUTE ON "DB"."DBA"."SPARQL_SINV_IMP" TO "SPARQL";
-
-# Importing namespace :
-DB.DBA.XML_SET_NS_DECL ('SBMLrdf', 'http://identifiers.org/biomodels.vocabulary#', 2);
-DB.DBA.XML_SET_NS_DECL ('bqbiol', 'http://biomodels.net/biology-qualifiers#', 2);
-DB.DBA.XML_SET_NS_DECL ('mnxCHEM', 'http://identifiers.org/metanetx.chemical/', 2);
-DB.DBA.XML_SET_NS_DECL ('chebi', 'http://identifiers.org/chebi/CHEBI:', 2);
-DB.DBA.XML_SET_NS_DECL ('fbc', 'http://www.sbml.org/sbml/level3/version1/fbc/version2#', 2);
-DB.DBA.XML_SET_NS_DECL ('model', 'http:doi.org/10.1126/scisignal.aaz1482#', 2);
-DB.DBA.XML_SET_NS_DECL ('cid', 'http://identifiers.org/pubchem.compound/', 2);
-DB.DBA.XML_SET_NS_DECL ('uniprot', 'http://identifiers.org/uniprot/', 2);
-DB.DBA.XML_SET_NS_DECL ('ncbigene', 'http://identifiers.org/ncbigene/', 2);
-DB.DBA.XML_SET_NS_DECL ('ensembl', 'http://identifiers.org/ensembl/', 2);
-DB.DBA.XML_SET_NS_DECL ('hgnc_symbol', 'http://identifiers.org/hgnc.symbol/', 2);
-DB.DBA.XML_SET_NS_DECL ('kegg_compound', 'http://identifiers.org/kegg.compound/', 2);
-DB.DBA.XML_SET_NS_DECL ('hmdb', 'http://identifiers.org/hmdb/', 2);
-DB.DBA.XML_SET_NS_DECL ('lipidmaps', 'http://identifiers.org/lipidmaps/', 2);
-DB.DBA.XML_SET_NS_DECL ('kegg_rdf', 'https://www.kegg.jp/entry/', 2);
-DB.DBA.XML_SET_NS_DECL ('cid_rdf', 'http://rdf.ncbi.nlm.nih.gov/pubchem/compound/CID', 2);
-DB.DBA.XML_SET_NS_DECL ('chebi_rdf', 'http://purl.obolibrary.org/obo/CHEBI_', 2);
-DB.DBA.XML_SET_NS_DECL ('chebi_2', 'https://www.ebi.ac.uk/chebi/searchId.do?chebiId=CHEBI:', 2);
-DB.DBA.XML_SET_NS_DECL ('chembl', 'http://identifiers.org/chembl.compound/', 2);
-DB.DBA.XML_SET_NS_DECL ('chembl_rdf', 'http://rdf.ebi.ac.uk/resource/chembl/molecule/', 2);
-
-DELETE FROM DB.DBA.RDF_QUAD ;
-
-ld_dir_all ('./dumps/MetaNetX/', 'metanetx.ttl', 'http://database/ressources/MetaNetX/4.0');
-ld_dir_all ('./dumps/MetaNetX/', 'HumanGEM.ttl', 'http://database/ressources/HumanGEM');
-
-ld_dir_all ('./dumps/vocabulary/', 'vocabulary_mesh_1.0.0.ttl', 'http://database/inference-rules');
-ld_dir_all ('./dumps/vocabulary/', 'skos.rdf', 'http://database/inference-rules');
-ld_dir_all ('./dumps/vocabulary/', 'fabio.ttl', 'http://database/inference-rules');
-ld_dir_all ('./dumps/vocabulary/', 'dublin_core_terms.ttl', 'http://database/inference-rules');
-ld_dir_all ('./dumps/vocabulary/', 'cito.ttl', 'http://database/inference-rules');
-ld_dir_all ('./dumps/vocabulary/', 'cheminf.owl', 'http://database/inference-rules');
-
-ld_dir_all ('./dumps/vocabulary/', 'chebi.owl', 'http://database/ressources/ChEBI');
-ld_dir_all ('./dumps/vocabulary/', 'ChemOnt_2_1.ttl', 'http://database/ressources/ChemOnt');
-
-select * from DB.DBA.load_list;
-rdf_loader_run();
-checkpoint;
-select * from DB.DBA.LOAD_LIST where ll_error IS NOT NULL;
-
-RDFS_RULE_SET ('schema-inference-rules', 'http://database/inference-rules');
-RDFS_RULE_SET ('schema-inference-rules', 'http://database/ressources/ChEBI');
-RDFS_RULE_SET ('schema-inference-rules', 'http://database/ressources/ChemOnt');
-
-
-checkpoint;
-
-EOF
-
     case $CMD in
         start)
             if [ -d ${DATA} ]; then
@@ -181,8 +99,6 @@ EOF
                 ${COMPOSE_CMD} up -d
                 waitStarted
             else
-				download_resources
-				#build
                 echo " -- Generating new compose instance."             
                 echo "---------------------------------" 
                 echo "$(ls)"
