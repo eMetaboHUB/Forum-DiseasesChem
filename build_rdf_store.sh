61a31682d5373e57d68a11454ee46865593df8cf
ofilangi <olivier.filangi@inrae.fr>
2020-09-22 13:59:19 +0000

    build_rdf - first step

---
+++ build_rdf_store.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+LOGSDIR=./logs-app
+RESOURCES_DIR=./share
+
+function runBuild() {
+
+CONTAINER_NAME=metdisease_app
+
+mkdir -p ${RESOURCES_DIR}
+mkdir -p ${LOGSDIR}
+
+COMPOSE_FILE=docker-compose-app.yml
+
+cat << EOF | tee ${COMPOSE_FILE} > /dev/null
+version: '3.3'
+services:
+    metdisease:
+        build: .
+        container_name: ${CONTAINER_NAME}
+        volumes:
+           - ${RESOURCES_DIR}:/workdir/share-virtuoso/
+           - ${LOGSDIR}:/workdir/out/
+        command: python3 app/build_RDF_store/build_RDF_store.py --config="app/build_RDF_store/config/config.ini"
+EOF
+
+docker-compose -f ${COMPOSE_FILE} build
+docker-compose -f ${COMPOSE_FILE} up
+rm -rf ${COMPOSE_FILE}
+}
+
+runBuild
