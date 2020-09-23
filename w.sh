d3b8f088ec751304538b2207a3eda99a083dc5f3
Nils Paulhe <nils.paulhe@inrae.fr>
2020-09-23 17:14:09 +0200

    [forum#16]  try fix CI (script path dir)

--- w.sh
+++ w.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+pushd $DIR
+
 # download/build rdf resources 
 ./w1_build_rdf_store.sh
 # download rdf resources 
 ./w1_2_pydio_files.sh F0rum_p455w0rd
 # start and load virtuoso with rdf data in the "share" directory
 ./w2_virtuoso.sh start
+
+popd
