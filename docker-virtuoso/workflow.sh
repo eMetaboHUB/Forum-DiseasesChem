667e330681ca738db68f0bcdb23a19df3cb034f4
Maxime Delmas <maxime.delmas@inra.fr>
2020-09-23 11:15:23 +0200

    modify import of metanetx

--- docker-virtuoso/workflow.sh
+++ docker-virtuoso/workflow.sh
@@ -38,7 +38,7 @@ echo "DB.DBA.XML_SET_NS_DECL ('chembl_rdf', 'http://rdf.ebi.ac.uk/resource/chemb
 # Start loading data :
 echo "DELETE FROM DB.DBA.RDF_QUAD ;" >> share/upload.sh
 
-echo "ld_dir_all ('./dumps/MetaNetX/', '*.ttl.gz', 'http://database/ressources/MetaNetX/4.0');" >> share/upload.sh
+echo "ld_dir_all ('./dumps/MetaNetX/', 'metanetx.ttl.gz', 'http://database/ressources/MetaNetX/4.0');" >> share/upload.sh
 
 echo "ld_dir_all ('./dumps/vocabulary/', 'vocabulary_mesh_1.0.0.ttl', 'http://database/inference-rules');" >> share/upload.sh
 echo "ld_dir_all ('./dumps/vocabulary/', 'skos.rdf', 'http://database/inference-rules');" >> share/upload.sh
