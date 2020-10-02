d13a4c59d3f58d2465aff70b6a8c221897268c4e
Olivier Filangi <olivier.filangi@gmail.com>
2020-10-02 15:21:17 +0200

    update 48GB config virtuos

--- w2_virtuoso.sh
+++ w2_virtuoso.sh
@@ -56,8 +56,8 @@ services:
         image: tenforce/virtuoso
         container_name: ${CONTAINER_NAME}
         environment:
-            VIRT_Parameters_NumberOfBuffers: 27200   # http://vos.openlinksw.com/owiki/wiki/VOS/VirtTipsAndTricksGuideRDFPerformanceTuning
-            VIRT_Parameters_MaxDirtyBuffers: 20000   # => Config 32GB 
+            VIRT_Parameters_NumberOfBuffers: 4000000   # http://vos.openlinksw.com/owiki/wiki/VOS/VirtTipsAndTricksGuideRDFPerformanceTuning
+            VIRT_Parameters_MaxDirtyBuffers: 3000000   # => Config 48GB 
             VIRT_Parameters_MaxCheckpointRemap: 680000
             VIRT_Parameters_TN_MAX_memory: 2000000000
             VIRT_SPARQL_ResultSetMaxRows: 10000000000
