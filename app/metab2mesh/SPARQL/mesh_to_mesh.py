1e5a4646d5f6b95668d16d0cc6d1d12486ff9e5a
Maxime Delmas <maxime.delmas@inra.fr>
2020-11-17 10:58:12 +0100

    add config

--- app/metab2mesh/SPARQL/mesh_to_mesh.py
+++ app/metab2mesh/SPARQL/mesh_to_mesh.py
@@ -16,19 +16,6 @@ prefix = """
     PREFIX obo: <http://purl.obolibrary.org/obo/>
 """
 
-count_number_of_MESH = """
-select (count(distinct ?mesh) as ?count_MESH)
-%s
-where 
-{
-    ?mesh a meshv:TopicalDescriptor .
-    ?mesh meshv:active 1 .
-    ?mesh meshv:treeNumber ?tn .
-    FILTER(REGEX(?tn,\"(C|A|G|F|I|J|D20|D23|D26|D27)\"))
-}
-"""
-
-
 count_distinct_pmids_by_MESH = """
 select ?MESH ?count
 %s
@@ -114,8 +101,8 @@ where
                                         order by ?mesh
                                     }
                                 }
-                                limit 5
-                                offset 10
+                                limit %s
+                                offset %s
                             }
                             ?endp obo:IAO_0000136 ?pmid .
                             ?pmid (fabio:hasSubjectTerm|fabio:hasSubjectTerm/meshv:hasDescriptor) ?mesh_ini .
@@ -129,7 +116,7 @@ where
                     ?mesh_ini_2 a meshv:TopicalDescriptor .
                     ?mesh_ini_2 meshv:active 1 .
                     ?mesh_ini_2 (meshv:treeNumber|meshv:treeNumber/meshv:parentTreeNumber+) ?tn_all .
-                    FILTER(REGEX(?tn_all,\"(C|A|G|F|I|J|D20|D23|D26|D27)\"))
+                    FILTER(REGEX(?tn_all,\"(C|A|G|F|I|J|D20|D23|D26|D27)\")) .
                     ?mesh_all meshv:treeNumber ?tn_all
                     FILTER
                         ( 
@@ -155,4 +142,58 @@ where
 }
 limit %d
 offset %d
+"""
+
+count_all_individuals = """
+select ?COUNT
+%s
+where
+{
+    {
+        select (count(distinct ?pmid) as ?COUNT)
+        where
+        {
+            {
+                select ?pmid 
+                where
+                {
+                    ?pmid a fabio:Article
+                }
+                limit %d
+                offset %d
+            }
+            ?endp obo:IAO_0000136 ?pmid .
+            ?pmid (fabio:hasSubjectTerm/meshv:treeNumber|fabio:hasSubjectTerm/meshv:hasDescriptor/meshv:treeNumber) ?tn .
+            FILTER(REGEX(?tn,\"(C|A|G|F|I|J|D20|D23|D26|D27)\")) .
+            ?mesh meshv:treeNumber ?tn .
+            ?mesh a meshv:TopicalDescriptor .
+            ?mesh meshv:active 1 .
+        }
+    }
+}
+limit %d
+offset %d
+"""
+
+
+count_number_of_MESH = """
+select (count(distinct ?mesh) as ?count_MESH)
+%s
+where 
+{
+    ?mesh a meshv:TopicalDescriptor .
+    ?mesh meshv:active 1 .
+    ?mesh meshv:treeNumber ?tn .
+    FILTER(REGEX(?tn,\"(C|A|G|F|I|J|D20|D23|D26|D27)\"))
+}
+"""
+
+
+count_all_pmids = """
+select count(?pmid) 
+%s
+where
+{
+    ?pmid a fabio:Article
+}
 """
