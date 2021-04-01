107b6f53271d7a7685133339c94a8e82765deba8
Maxime Delmas <maxime.delmas@inrae.fr>
2021-04-01 12:34:49 +0200

    fix upload met.net

--- workflow/w_upload_metabolic_network.sh
+++ workflow/w_upload_metabolic_network.sh
@@ -28,7 +28,6 @@ while getopts a:b:c:d:e:s:l: flag
 			s) RESOURCES_DIR=${OPTARG};;
 			l) LOGSDIR=${OPTARG};;
 			?) usage; exit 1;;
-
 	    esac
 	done
 
@@ -47,7 +46,7 @@ echo "1) Import SBML"
 LOG_SBML="${LOGSDIR}/load_SBML.log"
 echo "" > $LOG_SBML
 
-python3 app/SBML_upgrade/import_MetaNetX_mapping.py --config=$CONFIG --out=$RESOURCES_DIR --version=$SBML_VERSION
+python3 app/SBML_upgrade/import_MetaNetX_mapping.py --config=$CONFIG --out=$RESOURCES_DIR --version=$SBML_VERSION 2>&1 | tee -a $LOG_SBML
 
 echo "2) Import PubChem mapping"
 
@@ -55,7 +54,7 @@ echo "2) Import PubChem mapping"
 LOG_PUBCHEM="${LOGSDIR}/load_PubChem_mapping.log"
 echo "" > $LOG_PUBCHEM
 
-python3 app/SBML_upgrade/import_PubChem_mapping.py --config=$CONFIG --out=$RESOURCES_DIR --version=$PUBCHEM_VERSION
+python3 app/SBML_upgrade/import_PubChem_mapping.py --config=$CONFIG --out=$RESOURCES_DIR --version=$PUBCHEM_VERSION 2>&1 | tee -a $LOG_PUBCHEM
 
 
 echo "3) Import MetaNetX mapping"
@@ -64,6 +63,6 @@ echo "3) Import MetaNetX mapping"
 LOG_METANETX="${LOGSDIR}/load_MetaNetX_mapping.log"
 echo "" > $LOG_METANETX
 
-python3 app/SBML_upgrade/import_MetaNetX_mapping.py --config=$CONFIG --out=$RESOURCES_DIR --version=$METANETX_VERSION
+python3 app/SBML_upgrade/import_MetaNetX_mapping.py --config=$CONFIG --out=$RESOURCES_DIR --version=$METANETX_VERSION 2>&1 | tee -a $LOG_METANETX
 
 echo "End !"
