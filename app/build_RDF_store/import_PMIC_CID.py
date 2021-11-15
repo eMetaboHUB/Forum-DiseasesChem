921379b6d3771062d20bd6d6afc173f118a9b5fa
Maxime Delmas <maxime.delmas@inrae.fr>
2021-11-15 19:38:11 +0100

    init import_PMID_CID.py

---
+++ app/build_RDF_store/import_PMIC_CID.py
@@ -0,0 +1,38 @@
+import argparse, configparser, os, glob, sys, json, rdflib, eutils
+sys.path.insert(1, 'app/')
+from Elink_ressource_creator import Elink_ressource_creator
+from Database_ressource_version import Database_ressource_version
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--config", help="path to the configuration file")
+parser.add_argument("--out", help="path to output directory")
+parser.add_argument("--log", help="path to log and additional files directory")
+args = parser.parse_args()
+
+
+if not os.path.exists(args.config):
+    print("Config file : " + args.config + " does not exist")
+    sys.exit(3)
+
+try:    
+    config = configparser.ConfigParser()
+    config.read(args.config)
+except configparser.Error as e:
+    print(e)
+    sys.exit(3)
+
+# Creating the directory of all namespaces
+namespaces = {
+    "cito": rdflib.Namespace("http://purl.org/spar/cito/"),
+    "compound": rdflib.Namespace("http://rdf.ncbi.nlm.nih.gov/pubchem/compound/"),
+    "reference": rdflib.Namespace("http://rdf.ncbi.nlm.nih.gov/pubchem/reference/"),
+    "endpoint":	rdflib.Namespace("https://forum.semantic-metabolomics.org/mention/"),
+    "obo": rdflib.Namespace("http://purl.obolibrary.org/obo/"),
+    "dcterms": rdflib.Namespace("http://purl.org/dc/terms/"),
+    "fabio": rdflib.Namespace("http://purl.org/spar/fabio/"),
+    "mesh": rdflib.Namespace("http://id.nlm.nih.gov/mesh/"),
+    "void": rdflib.Namespace("http://rdfs.org/ns/void#"),
+    "skos": rdflib.Namespace("http://www.w3.org/2004/02/skos/core#"),
+    "source": rdflib.Namespace('http://rdf.ncbi.nlm.nih.gov/pubchem/source/'),
+    "concept": rdflib.Namespace('http://rdf.ncbi.nlm.nih.gov/pubchem/concept/')
+}
